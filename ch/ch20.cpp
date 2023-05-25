cc885518bc63b1c82b47a3e1fafe991b2a1db5c3
bcyoo <89197951+bcyoo@users.noreply.github.com>
2023-05-25 18:16:03 +0900

    배열

---
+++ ch/ch20.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main() {
+
+    // 배열
+    // 메모리가 연속적인 구조
+    int iArray[10] = {}; // 32bit에 0으로 채움
+    
+    iArray[4] = 10;
+
+    int iArray[100] = {};
+
+    
+    
+
+
+    
+    return 0;
+}
